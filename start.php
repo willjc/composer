5b5a06f669d5b8fd5e4e7a2be68ca0ab85379cd6
wangliansheng <8863704@qq.com>
2018-01-06 17:26:23 +0800

    测试composer引入成功与否

---
+++ start.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wangliansheng
+ * Date: 2018/1/6
+ * Time: 下午5:17
+ */
+require_once './vendor/autoload.php';
+$work=new Workerman\Worker("http://0.0.0.0:2333");
+$work->onMessage=function($connection,$data){
+    $connection->send("helloworld");
+};
+\Workerman\Worker::runAll();
+
